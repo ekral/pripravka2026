5127c6fc71a5dff1142332319554ede2a63e59da
Erik Král <ekral@utb.cz>
2026-08-19 13:17:51 +0200

    Update main.c

--- kvadraticka_rovnice/main.c
+++ kvadraticka_rovnice/main.c
@@ -3,15 +3,12 @@
 #include <math.h>
 
 int main(void) {
-    // m = x;
-    // n = x + 3;
-    // 40 m^2
-    // m * n = x * (x + 3) = 80
-    // x^2 + 3x - 80 = 0
+    double plocha = 100.0;
+    double vzdalenost = 3.0;
 
     const double a = 1.0;
-    const double b = 3.0;
-    const double c = -80.0;
+    const double b = vzdalenost;
+    const double c = -plocha;
 
     const double D = (b * b) - (4 * a * c);
 
@@ -19,18 +16,11 @@ int main(void) {
         const double x1 = (-b + sqrt(D)) / (2 * a);
         const double x2 = (-b - sqrt(D)) / (2 * a);
 
-        double x = 0.0;
-
-        // Priradte promenne x hodnotu x1 nebo x2 podle toho ktera je kladna
-        if (x1 > 0.0) {
-            x = x1;
-        }
-        else {
-            x = x2;
-        }
+        const double x = x1 > 0.0 ? x1 : x2;
 
         printf("Dum bude mit rozmery %.2f x %.2f\n", x, x);
         printf("Pozemek bude mit rozmery %.2f x %.2f\n", x, x + 3);
     }
+    
     return 0;
 }
