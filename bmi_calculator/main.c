726a4c6aa5537f4efe11238adb5bcbe4c31174e0
Erik Král <ekral@utb.cz>
2026-08-19 12:05:47 +0200

    Enhance BMI calculator with input validation and messages
    Added error handling for invalid height input and improved BMI categorization messages.

--- bmi_calculator/main.c
+++ bmi_calculator/main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-
+int main(void)
+{
     // TODO osetrit chybove stavy a neplatne hodnoty
-    // neco jsme zmenil
 
     char znaky[256];
 
     puts("Zadej vysku v centimetrech: ");
     fgets(znaky, sizeof(znaky), stdin);
     const long vyska = strtol(znaky, NULL, 10);
+
+    if (vyska <= 0) {
+        puts("Neplatna vyska.");
+        return 1;
+    }
 
     puts("Zadej hmotnost v kilogramech: ");
     fgets(znaky, sizeof(znaky), stdin);
@@ -24,7 +28,31 @@ int main(void) {
 
     printf("bmi: %.1f\n", bmi);
 
+    if (bmi >= 40.0) {
+        puts("Morbidni obezita.");
+    }
+    else if (bmi > 35.0) {
+        puts("Obezita 2. stupne.");
+    }
+    else if (bmi > 30.0) {
+        puts("Obezita 1. stupne.");
+    }
+    else if (bmi > 25.0) {
+        puts("Nadvaha.");
+    }
+    else if (bmi > 18.5) {
+        puts("Optimalni hmotnost.");
+    }
+    else if (bmi > 16.5) {
+        puts("Podvaha.");
+    }
+    else {
+        puts("Tezka podvyziva.");
+    }
+
+    puts("* Vypocet BMI je pouze orientacni, zavisi na veku, pohlavi a fyzicke kondici konkretniho cloveka.");
     puts("Konec programu, stiskni jakoukoliv klavesu pro konec.");
+
     getchar();
 
     return 0;
