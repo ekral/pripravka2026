ffc5af952fa0e8fcdcd37348f66540a99f04d39e
ekral <ekral@utb.cz>
2026-08-20 13:39:45 +0200

    add for

--- teplota_cpu/main.c
+++ teplota_cpu/main.c
@@ -2,7 +2,27 @@
 
 int main() {
 
-    int mereni[] = {35, 34, 38, 45, 70, 68, 50, 36};
+    const int mereni[] = {35, 34, 38, 45, 70, 68, 50, 36};
+
+    for (int i = 0; i < 8; ++i) {
+        const int teplota = mereni[i];
+        printf("%d\n", teplota);
+    }
+
+    int suma = mereni[0];
+
+    int index = 1;
+
+    while (index < 8)
+    {
+        const int teplota = mereni[index];
+        suma += teplota;
+        ++index;
+    }
+
+    const double prumer = (double)suma / 8;
+
+    printf("prumerna teplota CPU: %f\n", prumer);
 
     return 0;
 }
